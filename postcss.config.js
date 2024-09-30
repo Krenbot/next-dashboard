975e32080afd0872da818db8e59d6a87cbb9e86a
Steven Krenn <stevekrenn@gmail.com>
2024-09-29 20:34:47 -0500

    add skeleton for css module exports

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  theme: {
+    screens: {
+      // sm: '480px',
+      // md: '768px',
+      // lg: '976px',
+      // xl: '1440px',
+    },
+  },
+  variants: {},
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
