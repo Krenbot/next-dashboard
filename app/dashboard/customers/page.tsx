5228a17ccb214133a6dc6d13668b13a9a756a309
Steven Krenn <stevekrenn@gmail.com>
2024-09-29 20:32:48 -0500

    switch logo from acme to arch C&I

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,92 +3,95 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <>
-      {/* Project Completion */}
-      <div className="border-2 bg-white rounded flex flex-col justify-center w-1/2 flex flex-col items-center">
-        <h1 className="text-6xl uppercase text-[#ff671d] font-bold">
-          Project Completion
-        </h1>
-        <Image
-          src="/solar/test-solar.png"
-          width={500}
-          height={500}
-          alt="test solar"
-        />
-      </div>
+      <div className="flex flex-row text-center">
+        
+        {/* Project Completion */}
+        <div className="border-2 bg-white rounded flex flex-col justify-center  flex flex-col items-center">
+          <h1 className="text-6xl uppercase text-[#ff671d] font-bold">
+            Project Completion
+          </h1>
+          <Image
+            src="/solar/test-solar.png"
+            width={500}
+            height={500}
+            alt="test solar"
+          />
+        </div>
 
-      {/* Project Experts */}
-      <div className="border-2 bg-white rounded flex flex-col justify-center w-1/2">
-        <h1 className="text-6xl uppercase text-[#ff671d] font-bold">
-          Your Project Experts
-        </h1>
-        <table className="table-auto">
-          <thead className="bg-gray-50">
-            <tr className="text-center bg-cyan-950 border-1 text-white">
-              <th className="px-4 py-2">Department</th>
-              <th className="px-4 py-2">Contact</th>
-              <th className="px-4 py-2">Phone</th>
-              <th className="px-4 py-2">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="text-center">
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Admin
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Jillian Rooker
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                555-555-5555
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                jr@archsolar
-              </td>
-            </tr>
-            <tr className="text-center">
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Field
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Ryan Hakala
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                555-555-5555
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                rh@archsolar
-              </td>
-            </tr>
-            <tr className="text-center">
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Safety
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Josh Hooper
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                555-555-5555
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                jh@archsolar
-              </td>
-            </tr>
-            <tr className="text-center">
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                Marketing
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
-                JD Smith
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                555-555-5555
-              </td>
-              <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
-                jd@archsolar
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        {/* Project Experts */}
+        <div className="border-2 bg-white rounded flex flex-col justify-center">
+          <h1 className="text-6xl uppercase text-[#ff671d] font-bold">
+            Your Project Experts
+          </h1>
+          <table className="table-auto">
+            <thead className="bg-gray-50">
+              <tr className="text-center bg-cyan-950 border-1 text-white">
+                <th className="px-4 py-2">Department</th>
+                <th className="px-4 py-2">Contact</th>
+                <th className="px-4 py-2">Phone</th>
+                <th className="px-4 py-2">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-center">
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Admin
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Jillian Rooker
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  555-555-5555
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  jr@archsolar
+                </td>
+              </tr>
+              <tr className="text-center">
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Field
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Ryan Hakala
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  555-555-5555
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  rh@archsolar
+                </td>
+              </tr>
+              <tr className="text-center">
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Safety
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Josh Hooper
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  555-555-5555
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  jh@archsolar
+                </td>
+              </tr>
+              <tr className="text-center">
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  Marketing
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#1f3354]">
+                  JD Smith
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  555-555-5555
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-[#ff671d]">
+                  jd@archsolar
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
 
       {/* Progress Table */}
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,23 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
-    </div>
+    <>
+      <Image
+        src="/solar/20Logo-01-e1693253752458.png"
+        width={500}
+        height={500}
+        alt="Arch C&I Logo"
+      />
+    </>
+
+    // <div
+    //   className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    // >
+    //   <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+    //   <p className="text-[44px]">Acme</p>
+    // </div>
   );
 }
